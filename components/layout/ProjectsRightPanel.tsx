251092682b50a625730106a9aac1c32ab7b482ba
surryaansh <suryanshs1804@gmail.com>
2025-09-21 23:30:05 +0530

    Update ProjectsRightPanel.tsx

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -59,7 +59,7 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
         <img
           src={project.images[2]}
           alt={`${project.name} screenshot 3`}
-          className={`${imageBaseClasses} w-[22.5%] h-auto bottom-[8%] right-[20%]`}
+          className={`${imageBaseClasses} w-[22.5%] h-auto bottom-[18%] right-[30%]`}
           style={{ zIndex: 30 }}
           aria-hidden="true"
         />
