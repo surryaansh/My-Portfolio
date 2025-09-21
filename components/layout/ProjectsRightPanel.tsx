c853a40cd8a8032085966afafed13a90c73130f6
surryaansh <suryanshs1804@gmail.com>
2025-09-21 23:03:59 +0530

    Update ProjectsRightPanel.tsx

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -25,7 +25,6 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
     );
   }
 
-  // Base classes for images: no rounded corners, thin black border, no shadow.
   const imageBaseClasses = `absolute object-cover border ${isDarkMode ? 'border-neutral-800' : 'border-black'} transition-transform duration-300 hover:scale-[1.02]`;
 
   return (
@@ -36,44 +35,45 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
       </div>
       
       {/* 
-        The key change is here: The images are now in a container that takes up the full space,
-        not sharing it with a description. This allows for positioning based on the user's sketch.
+        This container is now `flex-1` and `relative`, serving as the canvas for the collage.
+        It will fill all available vertical space in its flex-column parent.
+        Padding has been removed from this container to allow images to align to the edges.
       */}
-      <div key={project.name} className="flex-1 relative w-full py-8 animate-fade-in" style={{ minHeight: '400px' }}>
+      <div key={project.name} className="flex-1 relative w-full h-full animate-fade-in">
         
-        {/* Image 1 (Bottom Left, Horizontal) */}
-        <img
-          src={project.images[0]}
-          alt={`${project.name} screenshot 1`}
-          className={`${imageBaseClasses} w-3/5 h-auto bottom-0 left-0`}
-          style={{ zIndex: 10 }}
-          aria-hidden="true"
-        />
-
-        {/* Image 2 (Top Center, Vertical) */}
-        <img
-          src={project.images[1]}
-          alt={`${project.name} screenshot 2`}
-          className={`${imageBaseClasses} w-2/5 h-auto top-[10%] left-[20%]`}
-          style={{ zIndex: 20 }}
-          aria-hidden="true"
-        />
-
-        {/* Image 4 (Top Right, Tall Vertical) */}
+        {/* Image 4 (Tall, Top Right) */}
         <img
           src={project.images[3]}
           alt={`${project.name} screenshot 4`}
-          className={`${imageBaseClasses} w-1/3 h-5/6 top-0 right-0 object-top`}
+          className={`${imageBaseClasses} w-[35%] h-[90%] top-0 right-0 object-top`}
           style={{ zIndex: 30 }}
           aria-hidden="true"
         />
 
-        {/* Image 3 (Middle Right, Horizontal) */}
+        {/* Image 2 (Vertical, Top Center) */}
+        <img
+          src={project.images[1]}
+          alt={`${project.name} screenshot 2`}
+          className={`${imageBaseClasses} w-[40%] h-auto max-h-[55%] top-[5%] left-[25%]`}
+          style={{ zIndex: 20 }}
+          aria-hidden="true"
+        />
+
+        {/* Image 3 (Horizontal, Middle Right) */}
         <img
           src={project.images[2]}
           alt={`${project.name} screenshot 3`}
-          className={`${imageBaseClasses} w-1/2 h-auto top-1/3 left-1/2 -translate-x-1/4`}
+          className={`${imageBaseClasses} w-[45%] h-auto max-h-[50%] top-[40%] left-[50%]`}
           style={{ zIndex: 40 }}
+          aria-hidden="true"
+        />
+        
+        {/* Image 1 (Horizontal, Bottom Left) */}
+        <img
+          src={project.images[0]}
+          alt={`${project.name} screenshot 1`}
+          className={`${imageBaseClasses} w-[55%] h-auto max-h-[50%] bottom-0 left-0`}
+          style={{ zIndex: 10 }}
           aria-hidden="true"
         />
 
